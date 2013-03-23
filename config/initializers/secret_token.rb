b0b975c4a29db1fa3eba4d983ca565f89473a1d3
Minh D <minhdn@outlook.com>
2013-03-22 21:36:05 -0700

    my commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hiruby::Application.config.secret_token = '9b30706f3cd09267a366e8d633dfb427639d734cf797dea47797d255998652a956f20d0e847da70ef9edccddfb3484d308f0d340d9b3c946bcfe1da7a1bf4615'
